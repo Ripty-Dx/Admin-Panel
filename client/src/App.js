e3dfd9adf1dffc19e8ce8129942374564ec05545
Ripty-Dx <riptyfullstack.dx@gmail.com>
2023-11-15 18:10:32 +0530

    client initial commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import "../node_modules/bootstrap/dist/js/bootstrap";
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
