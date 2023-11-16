94470268ee75552174569885005c493ec4500cab
Ripty-Dx <riptyfullstack.dx@gmail.com>
2023-11-16 14:05:16 +0530

    Employee UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
+import "../node_modules/bootstrap/dist/js/bootstrap";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import "../node_modules/bootstrap/dist/js/bootstrap";
+import Employees from "./components/employee/Employees";
+import AddNewEmployee from "./components/employee/AddNewEmployee";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/employees" element={<Employees />}></Route>
+        <Route path="/addNewEmployee" element={<AddNewEmployee />}></Route>
       </Routes>
     </>
   );
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+// import { Navigate } from "react-router-dom";
 
 const Home = () => {
+  // const navigate=Navigate();
+  const handleClick = (str) => {
+    if (str === "employee") {
+      window.location.href = "/employees";
+    }
+  };
   return (
     <>
       <div className="container-fluid">
-        <div className="row"></div>
+        <div className="row mt-5">
+          <div className="d-flex justify-content-center align-items-center">
+            <p className="m-0 fs-1 text-white fw-bold">ADMIN PANEL</p>
+          </div>
+        </div>
+        <div className="row mt-5">
+          <div className="d-flex justify-content-center align-items-center gap-4">
+            <div
+              className="bg-white border rounded d-flex justify-content-center align-items-center"
+              style={{ height: "150px", width: "250px", cursor: "pointer" }}
+              onClick={() => handleClick("employee")}
+            >
+              <p className="m-0 fs-4">Employee</p>
+            </div>
+            <div className="border bg-white rounded d-flex justify-content-center align-items-center" style={{ height: "150px", width: "250px", cursor: "pointer" }}>
+              <p className="m-0 fs-4">Company</p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
